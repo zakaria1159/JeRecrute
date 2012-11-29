3e689301201aaa60b89e2d0b453d4b1deafcf63e
zakaria1159 <z.boumarouane@gmail.com>
2012-11-29 17:56:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JeRecrute::Application.config.secret_token = '5e4b1471b07b90fc1a1923c2b7851616d85c0852624a25fb23550ab2e3acc10e3e413a506df89f1cd24ec0b8f9159c54d728d0de33ab1347f0ff1ed432ea3484'
